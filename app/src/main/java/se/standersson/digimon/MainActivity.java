b9bd3eafaa9fb30a1cdb2b47c024e8a0f271962e
MrSAndersson <stefan.nigma@gmail.com>
2017-02-01 22:11:13 +0100

    Minor changes

--- app/src/main/java/se/standersson/digimon/MainActivity.java
+++ app/src/main/java/se/standersson/digimon/MainActivity.java
@@ -3,7 +3,7 @@ package se.standersson.digimon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 import java.util.HashMap;
 
