440ea63edabafe3f1ac80210c5e5527a363df211
MrSAndersson <stefan.nigma@gmail.com>
2017-02-04 12:09:52 +0100

    Added validation of url entered and user notification for it

---
+++ app/src/main/java/se/standersson/digimon/MainTempFragment.java
@@ -0,0 +1,29 @@
+package se.standersson.digimon;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainTempFragment extends Fragment {
+
+
+    public MainTempFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_temp, container, false);
+    }
+
+}
