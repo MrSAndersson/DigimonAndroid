b39339b5c77046b0e1810c8bba11a180b6920596
MrSAndersson <stefan.nigma@gmail.com>
2017-03-05 17:40:47 +0100

    Notifications suspiciously working

--- app/src/main/java/se/standersson/icingalert/FirebaseCommunication.java
+++ app/src/main/java/se/standersson/icingalert/FirebaseCommunication.java
@@ -1,5 +1,9 @@
 package se.standersson.icingalert;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,5 +18,35 @@ public class FirebaseCommunication extends FirebaseMessagingService {
         // message, here is where that should be initiated.
         Log.d("FCM", "From: " + remoteMessage.getFrom());
         Log.d("FCM", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+
+        Intent resultIntent = new Intent(this, LoginActivity.class);
+// Because clicking the notification opens a new ("special") activity, there's
+// no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setContentIntent(resultPendingIntent);
+
+        notification.notify();
+
+// Sets an ID for the notification
+       // Integer mNotificationId = 1;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(1, notification.build());
     }
 }
